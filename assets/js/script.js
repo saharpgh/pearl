f098e59f2db7265a392e46742ec4661757713cb4
saharpgh <saharpourghasemi02@gmail.com>
2023-11-07 22:00:15 +0330

    complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,18 +30,52 @@ let stone11 = document.querySelector('.stone11')
 let stone12 = document.querySelector('.stone12')
 
 let bubble1 = document.querySelector('.bubble1')
-let bubble1top = bubble1.computedStyleMap().get('top').value
 let bubble2 = document.querySelector('.bubble2')
-let bubble2top = bubble2.computedStyleMap().get('top').value
 let bubble3 = document.querySelector('.bubble3')
-let bubble3top = bubble3.computedStyleMap().get('top').value
+let bubble4 = document.querySelector('.bibble4')
+let bubble5 = document.querySelector('.bibble5')
+let bubble6 = document.querySelector('.bibble6')
+let bubble7 = document.querySelector('.bubble7')
+let bubble8 = document.querySelector('.bubble8')
+let bubble9 = document.querySelector('.bubble9')
 
 
+let boll = document.querySelector('.boll')
+let fish3 = document.querySelector('.fish3')
+let seaanimal = document.querySelector('.seaanimal')
+let manyear = document.querySelector('.manyear')
+let cloudlast = document.querySelector('.cloudlast')
 
+const scrollableElement = document.querySelector('.scroll');
+let isHorizontalScroll = true;
+
+scrollableElement.addEventListener('wheel', (e) => {
+    if (isHorizontalScroll) {
+        e.preventDefault();
+        scrollableElement.scrollLeft += (e.deltaY + e.deltaX);
+
+        // Check if the horizontal scroll has reached the end
+        if (scrollableElement.scrollLeft >= (scrollableElement.scrollWidth - scrollableElement.clientWidth)) {
+            isHorizontalScroll = false;
+            enableVerticalScroll();
+        }
+    } else {
+        if (e.deltaY < 0) {
+            if (scrollableElement.scrollTop === 0) {
+                isHorizontalScroll = true;
+                e.preventDefault();
+            }
+        }
+    }
+});
+
+function enableVerticalScroll() {
+    // Add logic to enable vertical scrolling for the whole page
+}
 
 window.addEventListener('scroll', ()=> {
     let Sctop = window.scrollY;
-    console.log(Sctop)
+    console.log(scrollableElement.scrollTop)
     if (Sctop == 0) {
         MovingPearl.style.top =  '46%'
     }
@@ -97,7 +131,7 @@ window.addEventListener('scroll', ()=> {
         stone6.style.top = -340 + Sctop/6.927 + "vh"
         stone6.style.left = 120 - Sctop/41.9 + "%"
         stone7.style.top = -410 + Sctop/5.88 + "vh"
-        stone7.style.top = -100 + Sctop/18.36 + "%"
+        stone7.style.left = -100 + Sctop/18.36 + "%"
         stone8.style.top = -380 + Sctop/6.19 + "vh"
         stone8.style.left = 140 - Sctop/32.5 + "%"
         stone9.style.top = -400 + Sctop/5.89 + "vh"
@@ -111,6 +145,39 @@ window.addEventListener('scroll', ()=> {
     else if (Sctop>700 && Sctop<2000) {
         bubble1.style.top =  5100 - Sctop*2 + "px"
         bubble2.style.top =  3500 - Sctop + "px"
-        console.log(bubble1top)
+    }
+    else if (Sctop>900 && Sctop<3000) {
+        bubble3.style.top =  5900 - Sctop*1.3 + "px"
+    }
+    else if (Sctop>3200 && Sctop<4200) {
+        bubble5.style.top =  2000 - Sctop/2.3 + "px"
+        bubble6.style.top =  2000 - Sctop/2.5 + "px"
+        bubble4.style.top =  2000 - Sctop/2.1 + "px"
+        if (Sctop>3600 && Sctop<4200) {
+            console.log("djkfnkjedfkjen = " +boll.style.top)
+            boll.style.top = -1700 + Sctop/1.7 + "px"
+            boll.style.right = -5180 + Sctop*1.5 + "px"
+            fish3.style.top = -1490 + Sctop/2 + "px"
+            fish3.style.left = -1150 + Sctop/3 + "px"
+        }
+    }
+    else if (Sctop >4500 && Sctop < 5000) {
+        seaanimal.style.bottom = -5000 + Sctop + "px"
+        seaanimal.style.left = -4600 + Sctop + "px"
+    }
+    else if (Sctop >6300 && Sctop<6800) {
+        bubble7.style.top = -1040 + Sctop/2.3 + "px"
+        bubble8.style.top = 4850 - Sctop/2.3 + "px"
+        bubble9.style.top = -3100 + Sctop/1.3 + "px"
+        console.log(bubble9.style.top)
+    }
+    else if (Sctop >6800 && Sctop<7600) {
+        manyear.style.left =  -4530 + Sctop/1.5 + "px"
+        if (Sctop >6800 && Sctop<7000) {
+            manyear.style.top =  -533 + Sctop/3 + "px"
+        }
+    }
+    else if (Sctop >7930 && Sctop<8580) {
+        cloudlast.style.right = -8530 + Sctop/1 + "px"
     }
 })
